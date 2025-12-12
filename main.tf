90f37e9f1bd5ec53d2610ac4a5672ccc0184e312
taha <tahabutt6ix9ine@gmail.com>
2025-12-12 18:35:16 +0500

    Jenkins on port 8080, Nginx on port 8081

--- main.tf
+++ main.tf
@@ -9,11 +9,26 @@ terraform {
 
 provider "docker" {}
 
+# Nginx container - MOVED TO PORT 8081
 resource "docker_container" "nginx" {
   image = "nginx:latest"
   name  = "nginx_server"
   ports {
     internal = 80
-    external = 8080
+    external = 8081  # Nginx on 8081
+  }
+}
+
+# Jenkins container - ON PORT 8080
+resource "docker_container" "jenkins" {
+  image = "jenkins/jenkins:lts"
+  name  = "jenkins_server"
+  ports {
+    internal = 8080
+    external = 8080  # Jenkins on 8080
+  }
+  volumes {
+    host_path      = "C:\\jenkins_data"
+    container_path = "/var/jenkins_home"
   }
 }
